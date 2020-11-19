468daf57cb8774b21df87ac988d6f5b7d22a01b2
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-11-19 19:44:08 +0500

    UI completed

--- src/Components/AddTransaction.tsx
+++ src/Components/AddTransaction.tsx
@@ -5,8 +5,11 @@ const AddTransaction = () => {
     return (
         <div className="add-transaction">
             <p className="add-transaction-heading" >Add A Transaction</p>
-            <label>Name:</label>
-            <input type="text" className="add-transaction-text" />
+            <p className="add-transaction-heading">Name:</p>
+            <input type="text" className="add-transaction-text" placeholder="Enter A Transaction" />
+            <p className="add-transaction-amount-heading">Name:</p>
+            <input type="number" className="add-transaction-amount" placeholder="Enter An Amount" />
+            <button className="transaction-btn">Add Transaction</button>
         </div>
     )
 }
