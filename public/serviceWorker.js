8148ff5e754a69225a0555a3cf638dbfa93f1ec0
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-11-20 21:02:53 +0500

    created serviceWorker.js

---
+++ public/serviceWorker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "version1";
+
+const urlsToCache = ['index.html' , 'offline.html']
+
+const self = this;
+
+//Intsall SW
+
+self.addEventListener('install' ,(event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            console.log('Opened Cache');
+
+            return cache.addAll(urlsToCache)
+        })
+    )
+});
+
+//Listen For Request
+
+self.addEventListener('listen' ,(event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+                .catch((error) => caches.match('./offline.html'))
+        })
+    )
+});
+
+//Activates
+
+self.addEventListener('activate' ,(event) => {
+    const cacheWhiteList = [];
+
+    cacheWhiteList.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cachenames) => Promise.all(
+            // eslint-disable-next-line array-callback-return
+            cachenames.map((cachename) => {
+                if(!cacheWhiteList.includes(cachename)){
+                    return caches.delete(cachename);
+                }
+            })
+        ))
+    )
+});
